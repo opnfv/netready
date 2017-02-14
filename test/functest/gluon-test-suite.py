69a8847f21a811b7e3e6a883f36378c2ce76278b
Georg Kunz <georg.kunz@ericsson.com>
2017-02-14 16:49:52 +0100

    Adding empty framework for Gluon Functest suite
    Change-Id: I95652c38bc88b9ef29592a3f1f6d8160720a5cda
    Signed-off-by: Georg Kunz <georg.kunz@ericsson.com>

---
+++ test/functest/gluon-test-suite.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python
+
+import sys
+
+def main():
+    print "Running Gluon test suite..."
+    #
+    # TODO: awesome test
+    #
+    print "Gluon test suite successfully completed."
+    return 0
+
+if __name__ == '__main__':
+    sys.exit(main())
