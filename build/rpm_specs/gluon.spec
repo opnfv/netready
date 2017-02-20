a1c9d2ee18a543c0c2e3f3641ac7ff05f480bdb1
Georg Kunz <georg.kunz@ericsson.com>
2017-02-20 16:26:44 +0100

    Adding missing licence headers to files
    Change-Id: I9eedd2f3e21305a27fe09a116dec9d8dbcc6df5e
    Signed-off-by: Georg Kunz <georg.kunz@ericsson.com>

--- build/rpm_specs/gluon.spec
+++ build/rpm_specs/gluon.spec
@@ -1,3 +1,12 @@
+#
+# Copyright (c) 2016 Ericsson and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
 %define release 1
 %define _sharedstatedir /var/lib
 %define build_timestamp %(date +"%Y%m%d")
--- test/functest/gluon-test-suite.py
+++ test/functest/gluon-test-suite.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python
-
+#
+# Copyright (c) 2017 Ericsson and others.
+#
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Apache License, Version 2.0
+# which accompanies this distribution, and is available at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
 from opnfv.deployment import factory
 import functest.utils.openstack_utils as osutils
 import functest.utils.functest_logger as ft_logger
